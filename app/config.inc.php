99d2db4a37664646ef5d593caef176af19b8e9a9
Nicolea22 <leandrosuarez0@gmail.com>
2018-10-09 01:25:06 -0300

    db credentials

--- app/config.inc.php
+++ app/config.inc.php
@@ -4,3 +4,12 @@ $host = 'loca-lhost';
 $user = 'root';
 $password = '';
 $db_name = 'web';
+
+/*
+Para conectar a base de datos
+
+$host = 'ec2-54-225-68-133.compute-1.amazonaws.com';
+$user = 'atscpislttgwtd';
+$password = '96696fb9d3d42fcf94dbc16db98848b8cfcab23fd5237993050023c7228c0fbb';
+$db = 'dq87reoqrloaj';
+*/
